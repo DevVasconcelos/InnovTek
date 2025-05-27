d4469d97851f04de222ad0e7c237f8af2075aed0
DevVasconcelos <benhurarteagavasconcelos@gmail.com>
2025-05-27 16:18:46 -0300

    First Deploy

---
+++ Contato/scripts/contato.js
@@ -0,0 +1,13 @@
+document.getElementById('formContato').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const nome = document.getElementById('nome').value.trim();
+    const mensagem = document.getElementById('mensagem').value.trim();
+  
+    const texto = `Olá, meu nome é ${nome}. ${mensagem}`;
+    const numeroWhatsApp = '5512982223254'; // substitua pelo número real da empresa
+  
+    const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
+    window.open(url, '_blank');
+  });
+  
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+function goToPage(url) {
+  alert("Você será redirecionado para outra página!");
+  window.location.href = url;
+}
